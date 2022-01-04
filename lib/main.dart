ad38f265d503bb7228f10838b98429835c9c1ef4
shakilahammed055 <shakilahammed055@gmail.com>
2022-01-04 10:01:24 +0600

    firebase sign up

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 // import 'package:information/screens/add_user.dart';
 // import 'package:information/screens/user_list.dart';
@@ -6,7 +7,9 @@ import 'package:flutter/material.dart';
 // import 'package:information/screens/login_screen.dart';
 import 'package:information/screens/welcome.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
